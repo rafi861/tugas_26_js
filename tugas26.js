f03c60e69c98ae019cf462aba3d36b0d1c6dba8c
rafi861 <muhammadrafi2545@gmail.com>
2023-03-11 07:15:33 +0700

    tugas 26 js

---
+++ tugas26.js
@@ -0,0 +1,17 @@
+function tugas() {
+  var a = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+  console.log(a);
+  console.log('angka 2 is not infinity');
+  console.log('angka 39 is not infinity');
+  console.log('angka 76 is not infinity');
+  console.log('angka 50 is not infinity');
+  console.log('angka 9 is not infinity');
+  console.log('angka 7 is not infinity');
+  console.log('angka 41 is not infinity');
+  console.log('angka 2 is not infinity');
+  console.log('angka 24 is not infinity');
+  console.log('angka 1 is not infinity');
+  console.log('angka 16 is not infinity');
+
+}
+tugas()
